91ce65911034fd8d8aca0b0741b768fb2d3da864
Pratheep MM <pratheeepm@sahaj.ai>
2024-07-14 17:00:30 +0530

    ping - pong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,11 +8,23 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(open_stream) => {
-                println!("Connection established!");
+                handle_connection(open_stream);
             }
             Err(error) => {
                 println!("Error: {}", error);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let pong = "+PONG\r\n";
+    let ping = "PING";
+    let mut buf_reader = BufReader::new(&mut stream);
+    let request: Vec<String> = buf_reader.lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| line.contains(ping))
+        .collect();
+    println!("Received request {}", request.len());
+    stream.write_all(pong.as_bytes()).expect("unable to send response");
 }
